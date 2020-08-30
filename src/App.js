9754842d2bd36b0def49dd96970eff187adbce94
Deepacks <vladimirsette.vc@gmail.com>
2020-08-31 00:43:01 +0200

    v5.1
    -Fix: Backend link

--- src/App.js
+++ src/App.js
@@ -184,7 +184,7 @@ class App extends Component {
               <button onClick={() => {
                 window.location = window.location.href.includes('localhost')
                   ? 'http://localhost:8888/login'
-                  : 'https://vs-better-playlists-backend.herokuapp.com/Login' }
+                  : 'https://bps-vc-backend.herokuapp.com/login' }
               }
               style={{
                 'font-size': '50px',
